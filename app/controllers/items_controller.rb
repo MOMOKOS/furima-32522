445c1b5e0c8ded0452de5ce5735f2a89ec07d5c2
MOMOKOS <mmk.smz.1018@gmail.com>
2020-12-29 16:12:12 +0900

    itemsコントローラーのmove_to_indexメソッド修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,7 +54,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == @item.user_id
   end
 end
