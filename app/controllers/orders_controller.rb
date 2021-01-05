6b5ca78c29b00cc4f11b3f3c3d5ef16076912ee7
MOMOKOS <mmk.smz.1018@gmail.com>
2021-01-05 14:20:18 +0900

    購入機能のモデルとコントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,39 @@
+class OrdersController < ApplicationController
+  before_action :set_order
+  before_action :redirect_to_root_path
+  before_action :sold_out
+
+  def index
+    @order_destination = OrderDestination.new
+  end
+
+  def create
+    @order_destination = OrderDestination.new(order_destination_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_to_root_path
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def sold_out
+    if @item.order.present?
+      redirect_to root_path
+    end
+  end
+
+  def order_destination_params
+    params.require(:order_destination).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id:current_user.id, item_id:params[:item_id])
+  end
+
+end
