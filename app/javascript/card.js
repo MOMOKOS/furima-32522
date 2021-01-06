9e9f03ca2bc4ef8526968b9d1554f3fbaeae20af
MOMOKOS <mmk.smz.1018@gmail.com>
2021-01-06 17:02:29 +0900

    payjpのキーを環境変数に定義して呼び出すよう修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_88fe4509bd6319c14dcfe81a"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
