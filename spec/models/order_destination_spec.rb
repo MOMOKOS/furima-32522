720a1c02310a39cd137f61b9e492b8bc7e7c3713
MOMOKOS <mmk.smz.1018@gmail.com>
2021-01-06 11:51:11 +0900

    クレジットカード決済のテストコード

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe OrderDestination, type: :model do
         @order_destination = FactoryBot.build(:order_destination)
     end
 
-    it 'post_code,prefecture_id,city,address,phone_number,user_id,item_idを入力すると、商品の購入ができる' do
+    it 'post_code,prefecture_id,city,address,phone_number,user_id,item_id,tokenを入力すると、商品の購入ができる' do
       expect(@order_destination).to be_valid
     end
     it 'post_codeにハイフンが入っていないと登録できない' do
@@ -21,9 +21,12 @@ RSpec.describe OrderDestination, type: :model do
     end
     it 'phone_numberは11桁以内であれば登録できる' do
       @order_destination.phone_number = "111222333"
-      binding.pry
       expect(@order_destination).to be_valid
     end
-
+    it "tokenが空では登録できないこと" do
+      @order_destination.token = nil
+      @order_destination.valid?
+      expect(@order_destination.errors.full_messages).to include("Token can't be blank")
+    end
   end
 end
